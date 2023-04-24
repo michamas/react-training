158c8269093768a5bacf1cebb2e90fe4465c0a80
Michał Masiulanis <m.masiulanis@gmail.com>
2023-04-24 22:46:15 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import "./App.css";
-import React from "react";
-import FunctionContextComponent from "./FunctionContextComponent.js";
-import { ThemeProvider } from "./ThemeContext.js";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  // const renderCount = useRef(0);
+  const prevName = useRef("");
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
   return (
-    <ThemeProvider>
-      <FunctionContextComponent />
-    </ThemeProvider>
+    <>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <div>
+        My name is {name} and it used to be {prevName.current}
+      </div>
+      {/* <button onClick={focus}>Focus</button> */}
+    </>
   );
 }
---
+++ src/Context/AppContext.js
@@ -0,0 +1,12 @@
+// import "./App.css";
+// import React from "react";
+// import FunctionContextComponent from "./Context/FunctionContextComponent.js";
+// import { ThemeProvider } from "./Context/ThemeContext.js";
+
+// export default function App() {
+//   return (
+//     <ThemeProvider>
+//       <FunctionContextComponent />
+//     </ThemeProvider>
+//   );
+// }
